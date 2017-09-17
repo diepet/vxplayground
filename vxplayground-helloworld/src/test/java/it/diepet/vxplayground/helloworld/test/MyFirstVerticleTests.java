81e6b8da0faa4daa88779bc5e1e3bd5434517abc
Diego Pettisani <diepet@gmail.com>
2017-09-17 16:03:57 +0200

    Added vxplayground parent module and vxplayground-helloworld

---
+++ vxplayground-helloworld/src/test/java/it/diepet/vxplayground/helloworld/test/MyFirstVerticleTests.java
@@ -0,0 +1,40 @@
+package it.diepet.vxplayground.helloworld.test;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import io.vertx.core.Vertx;
+import io.vertx.ext.unit.Async;
+import io.vertx.ext.unit.TestContext;
+import io.vertx.ext.unit.junit.VertxUnitRunner;
+import it.diepet.vxplayground.helloworld.MyFirstVerticle;
+
+@RunWith(VertxUnitRunner.class)
+public class MyFirstVerticleTests {
+	private Vertx vertx;
+
+	@Before
+	public void setUp(TestContext context) {
+		vertx = Vertx.vertx();
+		vertx.deployVerticle(MyFirstVerticle.class.getName(), context.asyncAssertSuccess());
+	}
+
+	@After
+	public void tearDown(TestContext context) {
+		vertx.close(context.asyncAssertSuccess());
+	}
+
+	@Test
+	public void testMyApplication(TestContext context) {
+		final Async async = context.async();
+
+		vertx.createHttpClient().getNow(8080, "localhost", "/", response -> {
+			response.handler(body -> {
+				context.assertTrue(body.toString().contains("Hello"));
+				async.complete();
+			});
+		});
+	}
+}
