a2230c7bb37905b038f010f6ce71c85fbe9c6d65
Diego Pettisani <diepet@gmail.com>
2017-09-19 08:23:38 +0200

    Added log4j2 configuration

--- vxplayground-helloworld/src/main/java/it/diepet/vxplayground/helloworld/MyFirstVerticle.java
+++ vxplayground-helloworld/src/main/java/it/diepet/vxplayground/helloworld/MyFirstVerticle.java
@@ -1,13 +1,19 @@
 package it.diepet.vxplayground.helloworld;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Future;
 
 public class MyFirstVerticle extends AbstractVerticle {
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(MyFirstVerticle.class);
 
 	@Override
 	public void start(Future<Void> fut) {
 		vertx.createHttpServer().requestHandler(r -> {
+			LOGGER.info("Received some request");
 			r.response().end("<h1>Hello from my first " + "Vert.x 3 application</h1>");
 		}).listen(8080, result -> {
 			if (result.succeeded()) {
